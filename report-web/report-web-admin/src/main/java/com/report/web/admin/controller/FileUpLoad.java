5fdb058897b86bec05bd456a5e519b84da711bb5
lishun <960972314@qq.com>
2017-05-12 13:52:14 +0800

    shiro加入后,上传模块修改

--- report-web/report-web-admin/src/main/java/com/report/web/admin/controller/FileUpLoad.java
+++ report-web/report-web-admin/src/main/java/com/report/web/admin/controller/FileUpLoad.java
@@ -2,15 +2,17 @@ package com.report.web.admin.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.web.servlet.ShiroHttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -45,34 +47,37 @@ public class FileUpLoad {
 	 * @throws IOException
 	 */
 	@RequestMapping("/upload")
-	public String upload2(@RequestParam String upPath ,HttpServletRequest request, HttpServletResponse response)
+	public String upload2(HttpServletRequest request, HttpServletResponse response)
 			throws IllegalStateException, IOException {
+//	@RequestParam String upPath ,
+		log.info("upPath:[{}][{}]", request.getParameter("upPath"), request.getAttribute("upPath"));
+		String upPath = "/tpl/";
 		log.info("username[{}],upPath[{}]开始上传文件", SessionUtil.getUserInfo().getMember().getAccNo(), upPath);
 		String rootPath = request.getSession().getServletContext().getRealPath("/");
 		log.debug("获取到项目根路径为rootPath[{}]", rootPath);
-		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
-		// 判断 request 是否有文件上传,即多部分请求
-		if (multipartResolver.isMultipart(request)) {
-			// 转换成多部分request
-			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-			// 取得request中的所有文件名
-			Iterator<String> iter = multiRequest.getFileNames();
-			while (iter.hasNext()) {
-				long pre = System.currentTimeMillis();
-				MultipartFile file = multiRequest.getFile(iter.next());
-				if (file != null) {
-					if (StringUtils.isNotBlank(file.getOriginalFilename())) {
-						String fileName = StringUtil.combinationString(upPath, "/", file.getOriginalFilename());
-						String path = rootPath + fileName;
-						File localFile = new File(path);
-						file.transferTo(localFile);
-					}
+		
+		
+		ShiroHttpServletRequest shiroRequest = (ShiroHttpServletRequest) request;
+		CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+		MultipartHttpServletRequest multipartRequest = commonsMultipartResolver.resolveMultipart((HttpServletRequest) shiroRequest.getRequest());
+		
+		
+		Iterator<String> itr = multipartRequest.getFileNames();  
+	    MultipartFile file = null; 
+	    while (itr.hasNext()) {  
+	    	file = multipartRequest.getFile(itr.next());
+	        long pre = System.currentTimeMillis();
+			if (file != null) {
+				if (StringUtils.isNotBlank(file.getOriginalFilename())) {
+					String fileName = StringUtil.combinationString(upPath, "/", file.getOriginalFilename());
+					String path = rootPath + fileName;
+					File localFile = new File(path);
+					file.transferTo(localFile);
 				}
-				long finaltime = System.currentTimeMillis();
-				log.info("上传文件耗时:{}" , (finaltime - pre));
 			}
-
-		}
+			long finaltime = System.currentTimeMillis();
+			log.info("上传文件耗时:{}" , (finaltime - pre));
+	    }
 		return "file/success";
 	}
 }
