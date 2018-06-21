ea34e9805964269907f4f7e682d15830ca93b2fd
ls960972314 <960972314@qq.com>
2018-06-21 12:38:41 +0800

    delete all

--- report-common/report-common-dal/src/main/java/com/report/common/dal/query/dao/ReportCommonConDao.java
+++
@@ -1,9 +0,0 @@
-package com.report.common.dal.query.dao;
-
-/**
- * 
- * @author lishun
- *
- */
-public interface ReportCommonConDao {
-}
--- report-web/report-web-admin/src/main/webapp/WEB-INF/jsp/exception.jsp
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html> 
-<body>
-<div>
-    <h2>Something happened...</h2>
-    <p>${ex}</p>
-</div>
-</body>
-
-</html>
